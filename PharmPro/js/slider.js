2d75069ac767c529bc4e49e98dc8e1d2022ef166
Anarki <65044870+Anarki2314@users.noreply.github.com>
2022-10-18 13:33:13 +0300

    Add files via upload

--- PharmPro/js/slider.js
+++ PharmPro/js/slider.js
@@ -1,13 +1,13 @@
-const buttons=document.querySelectorAll('.advantages_slide_arrow')
+const buttons = document.querySelectorAll('.advantages_slide_arrow')
 buttons.forEach(button => {
-    button.addEventListener('click',function () {
-        const slides=document.querySelectorAll('.advantages_slide');
+    button.addEventListener('click', function () {
+        const slides = document.querySelectorAll('.advantages_slide');
         slides.forEach(slide => {
             slide.classList.toggle('active');
         });
-        const btns=document.querySelectorAll('.advantages_slide_arrow');
+        const btns = document.querySelectorAll('.advantages_slide_arrow');
         btns.forEach(btn => {
             btn.classList.toggle('active');
         });
-     })
+    })
 });
